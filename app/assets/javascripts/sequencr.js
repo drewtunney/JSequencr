b92ba0f0e1d0c9040bd0b853199a6465f5fb1eef
My Name <steven.a.weiss@gmail.com>
2014-03-19 19:28:46 -0400

    changed JS file to most recent, added buffer loader api

--- app/assets/javascripts/sequencr.js
+++ app/assets/javascripts/sequencr.js
@@ -1,47 +1,30 @@
 var context;
+var bufferLoader;
 window.addEventListener('load', init, false);
+
 function init() {
   try {
     // Fix up for prefixing
     window.AudioContext = window.AudioContext||window.webkitAudioContext;
     context = new AudioContext();
+
+    bufferLoader = new BufferLoader(
+      context,
+      [
+      'https://dl.dropboxusercontent.com/u/1316153/Cassette808_CL_01.wav',
+      'https://dl.dropboxusercontent.com/u/1316153/Cassette808_BD01.wav',
+      'https://dl.dropboxusercontent.com/u/1316153/Cassette808_Cow01.wav',
+      'https://dl.dropboxusercontent.com/u/1316153/Cassette808_BD01.wav',
+      ],
+      finishedLoading
+      );
+    bufferLoader.load();
   }
   catch(e) {
     alert('Web Audio API is not supported in this browser');
   }
 }
 
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-var context = new AudioContext();
-
-var snareBuffer = null;
-
-function loadSound(url) {
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
-  // Decode asynchronously
-  request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-      snareBuffer = buffer;
-      console.log(request);
-      playSound(snareBuffer);
-    }, onError);
-  };
-  request.send();
-}
-
-// function loadDogSound(url) {
-//  answer = $.get(url, function(data) {return data.decodeAudioData;});
-//  console.log($.parseXML(answer));
-// }
-
-function onError() {
-  alert("error");
-}
-
-// window.AudioContext = window.AudioContext || window.webkitAudioContext;
-// var context = new AudioContext();
 
 function playSound(buffer) {
   var source = context.createBufferSource(); // creates a sound source
@@ -49,6 +32,28 @@ function playSound(buffer) {
   source.connect(context.destination);      // connect the source to the context's destination (the speakers)
   source.start(0);                          // play the source now
                                             // note: on older systems, may have to use deprecated noteOn(time);
+}
+
+function finishedLoading(bufferList) {
+  // Create two sources and play them both together.
+  var source1 = context.createBufferSource();
+  var source2 = context.createBufferSource();
+  var source3 = context.createBufferSource();
+  var source4 = context.createBufferSource();
+  source1.buffer = bufferList[0];
+  source2.buffer = bufferList[1];
+  source3.buffer = bufferList[2];
+  source4.buffer = bufferList[3];
+
+
+  source1.connect(context.destination);
+  source2.connect(context.destination);
+  source3.connect(context.destination);
+  source4.connect(context.destination);
+  source1.start(0);
+  source2.start(1);
+  source3.start(2);
+  source4.start(3);
 }
 
 var snare = 'https://dl.dropboxusercontent.com/u/1316153/Cassette808_Snr02.wav';
@@ -70,14 +75,12 @@ $("#start").on("click", function(){
   }, 1000);
 });
 
-// function startSequencer(){
-//  console.log("helloooo");
-//  loadSound(snare);
-//  setInterval(playSound(snareBuffer), 4000);
-// }
 
 
 // https://dl.dropboxusercontent.com/u/1316153/Cassette808_CL_01.wav
 // https://dl.dropboxusercontent.com/u/1316153/Cassette808_BD01.wav
 // https://dl.dropboxusercontent.com/u/1316153/Cassette808_Cow01.wav
 // https://dl.dropboxusercontent.com/u/1316153/Cassette808_BD01.wav
+
+
+
