34cda4a332a7b37e11efeeab9fa7b2f69c28b4ea
tunneylax20 <bryantunney@gmail.com>
2014-03-20 12:16:13 -0400

    Grid set up + AWS getting response but keys not included

--- app/helpers/aws_helper.rb
+++ app/helpers/aws_helper.rb
@@ -1,12 +1,9 @@
 module AwsHelper
 
 
-
-
-Aws.config = { access_key_id: AWS_ACCESS_KEY_ID, 
-  secret_access_key: AWS_SECRET_ACCESS_KEY_ID,
-  region: AWS_REGION}
-
+  AWS_ACCESS_KEY_ID = ''
+  AWS_SECRET_ACCESS_KEY_ID = ''
+  AWS_REGION= ''
 
 
   def list_buckets
