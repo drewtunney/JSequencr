24f349158ef4b1a5b15d85e983c7f84ea733bfc3
iliastsangaris <iliastsangaris@gmail.com>
2014-03-20 14:59:32 -0400

    user can see a toggling visualizion of time in the grid

--- app/assets/javascripts/sequencer.js
+++ app/assets/javascripts/sequencer.js
@@ -1,3 +1,4 @@
+//create a transpose function
 function transpose(array) {
   return Object.keys(array[0]).map(
     function (column) {
@@ -5,6 +6,32 @@ function transpose(array) {
   );
 }
 
-  $("div.note").on("click", function(){
-    $(this).toggleClass("selected");
-  });
+//User can change color on click
+$("div.note").on("click", function(){
+  $(this).toggleClass("selected");
+});
+
+
+//Cyclying through the grid
+
+var intervalID = window.setInterval(gridCycle, 1000);
+var intervalCounter = 1;
+
+function gridCycle(){
+  $("div.column").children().removeClass("loop-timer");
+  if (intervalCounter > 4){
+    intervalCounter = 1;
+  }
+  console.log(intervalCounter);
+  $("div#column" + intervalCounter).children().toggleClass("loop-timer");
+  intervalCounter++;
+
+
+  // for (var intervalCounter = 1, intervalCounter < 5, intervalCounter++) {
+  //   var children = $("div#column" + intervalCounter).children();
+  //   children.toggleClass("loop-timer");
+  // }
+  // $("div#column1");
+  // intervalCounter++;
+  // console.log(intervalCounter);
+}
