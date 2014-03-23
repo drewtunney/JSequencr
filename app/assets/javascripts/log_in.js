d755f0df98132c5adc25ec09d04af43f70c5a3d1
My Name <steven.a.weiss@gmail.com>
2014-03-22 20:30:54 -0400

    login logout disappear

--- app/assets/javascripts/log_in.js
+++ app/assets/javascripts/log_in.js
@@ -4,17 +4,19 @@
 var createLoginForm = function(){
   $('#nav-bar').append(loginForm);
   $("<label>").text("Email:").appendTo(loginForm);
-  var loginEmailSpace = $("<input id='email' name='email'>").appendTo(loginForm);
+  var loginEmailSpace = $("<input id='login-email' name='email'>").appendTo(loginForm);
   $("<label>").text("Password:").appendTo(loginForm);
-  var loginPasswordSpace = $("<input id='password' type='password'>").appendTo(loginForm);
+  var loginPasswordSpace = $("<input id='login-password' type='password'>").appendTo(loginForm);
   $("<button id='log-in'>").appendTo(loginForm).text('Log In');
   $("#nav-bar").append(logoutButton);
+  loginSubmit();
+  logoutSubmit();
 };
 
 
 var login = function(){
-  var loginEmail = loginEmailSpace.val();
-  var loginPassword = loginPasswordSpace.val();
+  var loginEmail = $("input#login-email").val();
+  var loginPassword = $("input#login-password").val();
   console.log(loginEmail);
   $.ajax({
     type: 'POST',
@@ -25,9 +27,10 @@ var login = function(){
     },
     success: function(){
       $.get('/current_user', function(data){
-      name = data.name;
+      var name = data.name;
       $('body').append('<div>').append(name);
       loginForm.remove();
+      signUpForm.remove();
       });
     }
   });
@@ -44,21 +47,28 @@ var logout = function(){
     url: "/session",
     data: {
       'user_id': id
+    },
+    success: function(){
+      // createUserForm();
+      // createLoginForm();
     }
   });
   console.log("hello");
 };
 
-// var loginSubmit = function(){
+var loginSubmit = function(){
   $("#log-in").on("submit", function(e){
+    console.log("i'm trying to submit");
     e.preventDefault();
     login();
   });
-// };
+};
 
+var logoutSubmit = function(){
 $("#log-out").on("click", function(){
   logout();
 });
+};
 
 // createLoginForm();
 
