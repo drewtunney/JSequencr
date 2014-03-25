6e267842cee8bfca19d101bffdeeb054c92bb034
Zach Brady <zacbra@gmail.com>
2014-03-25 13:55:28 -0400

    fixed spec error re: user login email uniqueness; removed residual files relating to deleted sound entity

--- spec/models/sound_pattern_spec.rb
+++ spec/models/sound_pattern_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-  describe SoundPattern do 
-    it { should validate_presence_of :pattern }
-    it { should belong_to :sound }
-  end
+describe SoundPattern do 
+  it { should validate_presence_of :pattern }
+  it { should belong_to :song }
+end
--- spec/models/sound_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-  describe Sound do 
-    it { should validate_presence_of :name }
-    it { should validate_presence_of :url }
-    it { should have_many :sound_patterns }
-  end
