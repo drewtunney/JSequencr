91804af11eac33b034480606ff0ed231585e2115
My Name <steven.a.weiss@gmail.com>
2014-03-25 19:20:55 -0400

    tests added but do not pass

--- spec/features/user_can_sign_up.rb
+++ spec/features/user_can_sign_up.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
-describe "user can sign up" do
-  before(:all) do
-    Capybara.javascript_driver = :webkit
-  end
+describe "user can sign up", js: true do
+  # before(:all) do
+  #   Capybara.javascript_driver = :webkit
+  # end
 
-  it "can sign up", js: true do 
+  it "can sign up" do 
       visit root_path
-      
-
-      fill_in "name", with: "Steve"
-      fill_in "email", with: "Steve@steve.com"
-      fill_in "password", with: "swordfish"
-      fill_in "password_confirmation", with: "swordfish"
+      find("h2#sign-up-link").click
+      fill_in "signup-name", with: "Steve"
+      fill_in "signup-email", with: "Steve@steve.com"
+      fill_in "signup-password", with: "swordfish"
+      fill_in "signup-password-confirmation", with: "swordfish"
+      click_button "sign-up"
       # save_and_open_page
-      click_button "submit"
-      steve = User.find_by(name: "Steve")
-      expect(steve).to_not be_nil
+      # User.create({name: "Steve", email: "Steve", password: "pw", password_confirmation: "pw"})
+      # steve = User.find_by(name: "Steve")
+      expect(page).to have_content("Let's Get Weird")
       # expect(page).to have_content "Steve"
   end
 
