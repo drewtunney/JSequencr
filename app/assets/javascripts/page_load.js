31a3b913f6311fe9e24260ecb6780c60e213adf4
iliastsangaris <iliastsangaris@gmail.com>
2014-03-25 10:00:30 -0400

    fixed yield issue

--- app/assets/javascripts/page_load.js
+++ app/assets/javascripts/page_load.js
@@ -6,12 +6,9 @@ function settupDOM(){
   saver();
   
   songId = 0; //this means there is no song saved already
-  listSoundChoices();
-
   JSequencr.currentUserId = parseInt(JSequencr.currentUserId) || 0;
   
   addSignUpSubmitEventListener();
-  // updateSignUpForm();
   addLoginSubmitEventListener();
   addLogoutSubmitEventListener();
   updateLoginForm();
