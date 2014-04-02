ab64ca85ebc5223ef01dc2b773106915f483b928
tunneylax20 <bryantunney@gmail.com>
2014-04-02 05:59:11 -0400

    new form for icons

--- app/helpers/aws_helper.rb
+++ app/helpers/aws_helper.rb
@@ -16,7 +16,6 @@ module AwsHelper
 
   def list_sounds
     Aws.config = { access_key_id: AWS_ACCESS_KEY_ID, secret_access_key: AWS_SECRET_ACCESS_KEY_ID, region: AWS_REGION }
-    # binding.pry
     s3 = Aws::S3.new
     s3 = Aws.s3
     resp = s3.list_objects(bucket: 'Sounds')
@@ -24,7 +23,6 @@ module AwsHelper
     resp.contents.each do |song|
       songs[(song.key).downcase.split(".")[0]] = "https://s3.amazonaws.com/Sounds/#{song.key}"
     end
-
     songs
   end
 
